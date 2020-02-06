3b4c2d10e3c7d015c63451d3f9be01700602278a
rahim lotfi <sinuhe_1368@yahoo.com>
2020-02-06 16:10:28 +0330

    Initial commit

---
+++ WebApplication1/Infrastracture/BaseController.cs
@@ -0,0 +1,13 @@
+﻿//using System.Linq;
+//using System.Web.Mvc;
+
+namespace Infrastracture
+{
+    public class BaseController:System.Web.Mvc.Controller
+    {
+        public BaseController():base()
+        {
+
+        }
+    }
+}
