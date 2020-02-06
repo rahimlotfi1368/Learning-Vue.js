73731e17d51963ba11ad0336d96e0125223d7a73
rahim lotfi <sinuhe_1368@yahoo.com>
2020-02-06 18:28:41 +0330

    creating template

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
+﻿using System.Web.Mvc;
 
 namespace WebApplication1
 {
-    public class RouteConfig
+    public static class RouteConfig
     {
-        public static void RegisterRoutes(RouteCollection routes)
+         static RouteConfig()
+        {
+
+        }
+        public static void RegisterRoutes(System.Web.Routing.RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = System.Web.Mvc.UrlParameter.Optional }
             );
         }
     }
