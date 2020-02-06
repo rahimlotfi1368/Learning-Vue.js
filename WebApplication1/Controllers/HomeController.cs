978875c557b1d78289d31c785ccb844d3e8fed82
rahim lotfi <sinuhe_1368@yahoo.com>
2020-02-06 17:06:50 +0330

    instaling nugets

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿//using System.Linq;
+//using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public partial class HomeController : Infrastracture.BaseController
+    {
+        public HomeController() : base()
+        {
+
+        }
+        [System.Web.Mvc.HttpGet]
+        public virtual System.Web.Mvc.ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
